fdce06640b7c731a32662e986a95a799abc45ced
unknown <heliosmaxim@gmail.com>
2022-09-19 00:18:16 -0700

    ex_36.py

---
+++ ex_36.py
@@ -0,0 +1,11 @@
+def main():
+    r_d = float(input())
+    d_e = float(input())
+    forming = 1 / r_d / d_e
+    res = round(forming, 2)
+    print(res)
+    
+
+
+if __name__ == "__main__":
+    main()
